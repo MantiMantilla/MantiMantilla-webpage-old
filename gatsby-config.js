8ff90e0b80c8befb2c4866e6556dbbfdd24db53f
MantiMantilla <alejandromantillaredondo@gmail.com>
2020-11-20 09:40:35 -0500

    add dark theme dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-typescript`,
+    `gatsby-plugin-dark-mode`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
