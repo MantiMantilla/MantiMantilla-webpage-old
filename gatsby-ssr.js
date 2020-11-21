0239202a5e1d2f0f1b252af7d09e4dba72f54710
MantiMantilla <alejandromantillaredondo@gmail.com>
2020-11-21 03:35:20 -0500

    Moved head scripts for SEO and Adsense to gatsby-ssr.js

---
+++ gatsby-ssr.js
@@ -0,0 +1,25 @@
+const React = require("react")
+
+const HeadComponents = [
+    /*Google Analytics Scripts (Google Search Console)*/
+    <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-PHR3L63QNF">
+    </script>,
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+        gtag('config', 'G-PHR3L63QNF');
+    </script>,
+    /*Google Adsense Script*/
+    <script
+        data-ad-client="ca-pub-7391945618852590"
+        async
+        src="https://pagead2.googlesyndication.com.co/pagead/js/adsbygoogle.js">
+    </script>
+]
+
+exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+  setHeadComponents(HeadComponents)
+}
