53add8fa66e99d5349f47526666abbd76573aa0a
MantiMantilla <alejandromantillaredondo@gmail.com>
2020-11-21 04:26:47 -0500

    Revert "Update gatsby-ssr.js"
    This reverts commit 411334b138ed3111c40d084627e46f0161317e12.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -16,7 +16,7 @@ const HeadComponents = [
     <script
         data-ad-client="ca-pub-7391945618852590"
         async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+        src="https://pagead2.googlesyndication.com.co/pagead/js/adsbygoogle.js">
     </script>
 ]
 
