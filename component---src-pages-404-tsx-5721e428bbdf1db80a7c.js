297650ace62fd3979f1b89467f5238b5cd2bd926
MantiMantilla <MantiMantilla@users.noreply.github.com>
2023-09-14 02:24:21 +0000

    deployed via Gatsby Publish Action 🎩 for 6f5dfd668e791b9b527d878cd0d0e22851e0817c

---
+++ component---src-pages-404-tsx-5721e428bbdf1db80a7c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"i6+/":function(t,e,a){"use strict";a.r(e);var n=a("q1tI"),s=a.n(n),c=a("Zttt"),l=a("wtQ5"),o=a("OKcn"),u=a("q5BQ");e.default=()=>s.a.createElement(c.a,null,s.a.createElement(l.a,{title:"404: Not found"}),s.a.createElement(o.a,{section:!0},s.a.createElement(u.a,{title:"404",subtitle:"Page not found",center:!0}),s.a.createElement("p",{className:"mt-4 text-center w-full"},"You just hit a route that doesn't exist... the sadness.")))}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-5721e428bbdf1db80a7c.js.map
